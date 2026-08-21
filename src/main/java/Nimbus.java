cd716d3c06bc60fde0fa304d7a998969bc51f1e5
Rohita-29 <rohita.kataru@gmail.com>
2026-08-21 15:29:59 +0800

    Add greeting and exit messages

--- src/main/java/Nimbus.java
+++ src/main/java/Nimbus.java
@@ -1,10 +1,14 @@
 public class Nimbus {
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String banner = " _   _ _           _               \n"
+                + "| \\ | (_)_ __ ___ | |__  _   _ ___ \n"
+                + "|  \\| | | '_ ` _ \\| '_ \\| | | / __|\n"
+                + "| |\\  | | | | | | | |_) | |_| \\__ \\\n"
+                + "|_| \\_|_|_| |_| |_|_.__/ \\__,_|___/\n";
         System.out.println(banner);
+        String name = "Nimbus";
+        System.out.println("Hello! I'm " + name + ".");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
