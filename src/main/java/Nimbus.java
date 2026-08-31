7da0b37859fe052807122a4604d9582dcc16853a
Rohita-29 <rohita.kataru@gmail.com>
2026-08-31 19:00:51 +0800

    Add unmark command to revert task done status

--- src/main/java/Nimbus.java
+++ src/main/java/Nimbus.java
@@ -17,20 +17,25 @@ public class Nimbus {
         int taskCount =0;
 
         Scanner scanner = new Scanner(System.in);
-        while (true){
-            String input= scanner.nextLine();
-            if(input.equals("bye")){
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
                 break;
-            }else if (input.equals("list")) {
+            } else if (input.equals("list")) {
                 for (int i = 0; i < taskCount; i++) {
-                    String icon = isDone[i] ? "X":" ";
-                    System.out.println((i + 1) + ".["+icon+"]" + tasks[i]);
+                    String icon = isDone[i] ? "X" : " ";
+                    System.out.println((i + 1) + ".[" + icon + "]" + tasks[i]);
                 }
-            }else if (input.startsWith("mark ")) {
+            } else if (input.startsWith("mark ")) {
                 int index = Integer.parseInt(input.substring(5)) - 1;
                 isDone[index] = true;
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.println("  [X] " + tasks[index]);
+            } else if(input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.substring(7)) - 1;
+                isDone[index] = false;
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  [ ] " + tasks[index]);
             }else{
                     tasks[taskCount]=input;
                     taskCount++;
