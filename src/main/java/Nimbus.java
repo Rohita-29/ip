8a3397490b0c9f03fec3c5d4f640dfcd5ba4b23f
Rohita-29 <rohita.kataru@gmail.com>
2026-08-31 19:07:01 +0800

    Introduce Task class to represent tasks

--- src/main/java/Nimbus.java
+++ src/main/java/Nimbus.java
@@ -12,8 +12,7 @@ public class Nimbus {
         System.out.println("Hello! I'm " + name + ".");
         System.out.println("What can I do for you?");
 
-        String[] tasks = new String[100];
-        boolean[] isDone = new boolean[100];
+        Task[] tasks = new Task[100];
         int taskCount =0;
 
         Scanner scanner = new Scanner(System.in);
@@ -23,23 +22,22 @@ public class Nimbus {
                 break;
             } else if (input.equals("list")) {
                 for (int i = 0; i < taskCount; i++) {
-                    String icon = isDone[i] ? "X" : " ";
-                    System.out.println((i + 1) + ".[" + icon + "]" + tasks[i]);
+                    System.out.println((i + 1) + "." + tasks[i]);
                 }
             } else if (input.startsWith("mark ")) {
                 int index = Integer.parseInt(input.substring(5)) - 1;
-                isDone[index] = true;
+                tasks[index].markAsDone();
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println("  [X] " + tasks[index]);
+                System.out.println("  " + tasks[index]);
             } else if(input.startsWith("unmark ")) {
                 int index = Integer.parseInt(input.substring(7)) - 1;
-                isDone[index] = false;
+                tasks[index].markAsNotDone();
                 System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("  [ ] " + tasks[index]);
+                System.out.println("  " + tasks[index]);
             }else{
-                    tasks[taskCount]=input;
+                    tasks[taskCount] = new Task(input);
                     taskCount++;
-                    System.out.println("added: "+ input);
+                    System.out.println("added: "+input);
                 }
             }
         System.out.println("Bye! Hope to see you again!");
